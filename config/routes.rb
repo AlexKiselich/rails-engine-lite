be4aa573e4d71beeddc41ad8d4e61ae2f10c3b2b
Alex Kiselich <alexander.kiselich@gmail.com>
2023-07-11 19:14:05 -0600

    App set up

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # get "/api/v1/merchants"
+
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], controller: "merchants/items"
+      end
+      resources :items
+    end
+  end
+end
